39ae4a9351e064ee7eb31cf88ac78ef8c7842dc7
Kevin Hanna <kevin.hanna@whereby.com>
2024-12-19 18:09:41 +0000

    I seeee

--- src/client.py
+++ src/client.py
@@ -58,6 +58,7 @@ class Client:
         )
 
     def append_audio_data(self, audio_data):
+        print(f"append {len(audio_data)}")
         self.buffer.extend(audio_data)
 
     def process_audio(self, websocket, vad_pipeline, asr_pipeline):
